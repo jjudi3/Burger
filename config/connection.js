0a02ba0ac461097d32b70a13651861ca4c674f4c
jjudi3 <j_vperez29@bellsouth.net>
2018-04-25 18:16:56 -0400

    Add files via upload

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-var connection;
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-  }else {
-    connection = mysql.createConnection ({
-      host: "localhost", 
-      user: "root", 
-      password: "Star$h1p",
-      database: "burgers_db"
-    });
-  };
-// Make connection.
-connection.connect();
-// Export connection for our ORM to use.
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: process.env.db_host || "localhost",
+    user: process.env.db_user || "root",
+    password: process.env.db_pw || "Star$h1p",
+    database: process.env.db || "burgers_db"
+});
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
 module.exports = connection;
